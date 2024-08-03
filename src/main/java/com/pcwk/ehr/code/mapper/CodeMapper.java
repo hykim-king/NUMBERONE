30ef78857a9b9b8b8074e519f77bf669cdace855
C.H.Hwang <chanho0043@naver.com>
2024-08-03 11:43:00 +0900

    code

---
+++ src/main/java/com/pcwk/ehr/code/mapper/CodeMapper.java
@@ -0,0 +1,15 @@
+package com.pcwk.ehr.code.mapper;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.pcwk.ehr.cmn.WorkDiv;
+import com.pcwk.ehr.code.domain.Code;
+
+public interface CodeMapper extends WorkDiv<Code> {
+
+	List<Code> doRetrieveIn(List search) throws SQLException;
+		
+	
+	
+}
