4da10916448158a8f8e11f6a70a61e3a94fefee5
S.J. Kim <tmdwns628@naver.com>
2024-08-21 17:37:08 +0900

    board reply edit

--- src/main/java/com/pcwk/ehr/board/service/BoardServiceImpl.java
+++ src/main/java/com/pcwk/ehr/board/service/BoardServiceImpl.java
@@ -1,11 +1,16 @@
-
 package com.pcwk.ehr.board.service;
 
 import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.pcwk.ehr.board.domain.Board;
 import com.pcwk.ehr.cmn.DTO;
@@ -45,31 +50,46 @@ public class BoardServiceImpl implements BoardService, PLog {
 		return boardMapper.doSave(inVO);
 	}
     
-	 @Override
-	 public int askCntUpdate(Board inVO) throws SQLException {
-	     log.debug("1. param :" + inVO);
-	     return boardMapper.askCntUpdate(inVO);
-	 }
+	@Override
+	public int askCntUpdate(Board inVO) throws SQLException {
+	    log.debug("1. param :" + inVO);
+	    return boardMapper.askCntUpdate(inVO);
+	}
 	
+	
+	 
 	//조회 count증가
 	@Override
-	public Board doSelectOne(Board inVO) throws SQLException {
-		//단건 조회
-		log.debug("1. param :"+inVO);
-		Board outVO = boardMapper.doSelectOne(inVO);
-		log.debug("2. outVO :"+outVO);
-		
-		int flag = 0;
-		//조회 count증가
-		if(null != outVO) {
-            flag = boardMapper.readCntUpdate(inVO);
-            log.debug("3. 조회 count증가 :" + flag);
+    public Board doSelectOne(Board inVO) throws SQLException {
+        //단건 조회
+        log.debug("1. param :"+inVO);
+        Board outVO = boardMapper.doSelectOne(inVO);
+        log.debug("2. outVO :"+outVO);
+        
+        int flag = 0;
+        //조회 count증가
+        if(null != outVO && inVO.getRegId() != null) {
+            // 세션에서 조회한 게시글 목록을 가져옴
+            HttpSession session = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession();
+            Set<Integer> viewedPosts = (Set<Integer>) session.getAttribute("viewedPosts");
+            if (viewedPosts == null) {
+                viewedPosts = new HashSet<>();
+                session.setAttribute("viewedPosts", viewedPosts);
+            }
             
-            if(1 == flag) {
-                outVO.setReadCnt(outVO.getReadCnt() + 1);
+            // 이미 조회한 게시글이 아닌 경우에만 조회수 증가
+            if (!viewedPosts.contains(inVO.getBoardNo())) {
+                flag = boardMapper.readCntUpdate(inVO);
+                log.debug("3. 조회 count증가 :" + flag);
+                
+                if(1 == flag) {
+                    outVO.setReadCnt(outVO.getReadCnt() + 1);
+                    viewedPosts.add(inVO.getBoardNo());
+                }
             }
         } 
-		return outVO;
-	}
+        return outVO;
+    }
+	
 
 }
